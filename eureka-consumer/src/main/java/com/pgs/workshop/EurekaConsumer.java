74b63d2c2b7ae34659de2ee9b863d817701a8a99
Bartosz Miedlar <bmiedlar@pgs-soft.com>
2018-04-19 10:45:18 +0200

    Part 3 - Communication between Eureka clients

---
+++ eureka-consumer/src/main/java/com/pgs/workshop/EurekaConsumer.java
@@ -0,0 +1,23 @@
+package com.pgs.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaConsumer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumer.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
